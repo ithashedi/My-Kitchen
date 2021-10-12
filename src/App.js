e89bf0d79c49d2aa9697065272b75b4f54ada8a7
Muhammed Hashedi <71554865+ithashedi@users.noreply.github.com>
2021-10-12 11:07:25 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import "./styles.css";
+
+// this is a component for the header
+function Header(props) {
+  return <h1>{props.name} Kitchen</h1>;
+}
+
+// creating the component for the main
+function Main(props) {
+  return <h2> we serve the {props.adjective} food</h2>;
+}
+
+// creating the footer
+function Footer(props) {
+  return <h3>Copy rights {props.year}</h3>;
+}
+export default function App() {
+  return (
+    <div className="App">
+      <Header name="Muhammed" />
+      <Main adjective="best" />
+      <Footer year={new Date().getFullYear()} />
+    </div>
+  );
+}
